c6393c0c512eaa0f4c5d4a23d3c86630b0d42601
Nikolas <nikolastroupkos@gmail.com>
2025-09-20 14:06:43 -0300

    Add JWT authentication filter and update application properties for routing

---
+++ api-gateway/src/main/java/com/backend/api_gateway/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,71 @@
+package com.backend.api_gateway.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String path = request.getURI().getPath();
+        System.out.println("Request path: " + path);
+
+
+        //TODO agregar las rutas que queden
+        if (path.equals("/auth/login") || path.equals("/auth/register") || path.equals("/users")
+                || path.startsWith("/swagger") || path.startsWith("/api-docs")) {
+            return chain.filter(exchange);
+        }
+
+        // Verificar header Authorization
+        String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(secret.getBytes(StandardCharsets.UTF_8))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+
+            request = exchange.getRequest().mutate()
+                    .header("X-User-Id", claims.getSubject())
+                    .build();
+            exchange = exchange.mutate().request(request).build();
+
+        } catch (Exception e) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
